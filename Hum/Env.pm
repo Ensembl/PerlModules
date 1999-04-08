c578013dfb87211713303c5b29481076f392146f
James Gilbert <jgrg@sanger.ac.uk>
1999-04-08 13:47:02 +0000

    Env.pm now works as advertised. Tim additions and bugfixes to SDTable

--- Hum/Env.pm
+++ Hum/Env.pm
@@ -1,43 +1,57 @@
 
+package Hum::Env;
+
 =pod
 
 =head1 NAME Hum::Env
 
 =head1 DESCRIPTION
 
-B<Hum::Env> sets up the human analysis
-environment for a script.  It is used in cron
-jobs and CGI scripts to ensure that B<PERL5LIB>
-and B<PATH> environment variables, etc... are set
-up correctly.
+B<Hum::Env> is used to provide a controlled
+environment for scripts in the human analysis
+system.  It is used in cron jobs and CGI scripts
+to ensure that B<PERL5LIB> and B<PATH>
+environment variables, etc... are set up
+correctly.
+
+=head1 SYNOPSIS
+
+    use Hum::Env;
+
+B<Hum::Env> empties the current B<%ENV> hash,
+thus simulating the cron or CGI environment on
+the command line, with the exception that
+B<PERL5LIB> in your current environment will have
+already added entries to B<@INC>.
+
+Remember that you need to "use lib <path>", where
+B<path> contains Hum::Env.
+
+=head2 Author
+
+James Gilbert email B<jgrg@sanger.ac.uk>
 
 =cut
 
-package Hum::Env;
-
 use strict;
 
-my (@path, $script, $fullScript);
+my (@libs, @path);
 
-# Location of PERL5LIB's
-$ENV{'PERL5LIB'} = join ':', qw(
-				/nfs/disk100/humpub/modules/PerlModules
-				/nfs/disk100/humpub/modules
-				/usr/local/badger/bin
-				);
+# Set up @INC
+BEGIN {
+    @libs = qw(
+	       /nfs/disk100/humpub/modules/PerlModules
+	       /nfs/disk100/humpub/modules
+	       /usr/local/badger/bin
+	       );
+}
+use lib @libs;
 
-# For scripts which access oracle
-$ENV{'ORACLE_HOME'} = '/usr/local/oracle';
-$ENV{'TWO_TASK'}    = 'sids';
+# Empty the environment
+%ENV = ();
 
-# For blast
-$ENV{BLASTDB}       = '/nfs/disk100/humpub/blast';
-$ENV{BLASTFILTER}   = '/usr/local/pubseq/bin';
-$ENV{BLASTMAT}      = '/nfs/disk100/pubseq/blastdb';
-$ENV{NCBI}          = '/nfs/disk100/pubseq/blastdb';
-
-# For halfwise
-$ENV{WISECONFIGDIR} = '/nfs/disk100/pubseq/wise/wisecfg';
+# Make @INC get propagated to system calls
+$ENV{'PERL5LIB'} = join ':', @libs;
 
 # List of dirs for PATH
 @path = qw(
@@ -61,14 +75,19 @@ $ENV{WISECONFIGDIR} = '/nfs/disk100/pubseq/wise/wisecfg';
 # Set PATH environment
 $ENV{'PATH'} = join ':', @path;
 
+# For scripts which access oracle
+$ENV{'ORACLE_HOME'} = '/usr/local/oracle';
+$ENV{'TWO_TASK'}    = 'sids';
+
+# For blast
+$ENV{BLASTDB}       = '/nfs/disk100/humpub/blast';
+$ENV{BLASTFILTER}   = '/usr/local/pubseq/bin';
+$ENV{BLASTMAT}      = '/nfs/disk100/pubseq/blastdb';
+$ENV{NCBI}          = '/nfs/disk100/pubseq/blastdb';
+
+# For halfwise
+$ENV{WISECONFIGDIR} = '/nfs/disk100/pubseq/wise/wisecfg';
+
 1;
 
 __END__
-
-=head1 SYNOPSIS
-
-    use Hum::Env;
-
-=head2 Author
-
-James Gilbert email B<jgrg@sanger.ac.uk>
