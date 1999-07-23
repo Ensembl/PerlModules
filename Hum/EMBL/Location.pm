35b4f23e8d743bb009d34f47fdb7f8042577a4ae
James Gilbert <jgrg@sanger.ac.uk>
1999-07-23 18:43:17 +0000

    *** empty log message ***

--- Hum/EMBL/Location.pm
+++ Hum/EMBL/Location.pm
@@ -5,7 +5,119 @@ use strict;
 use Exporter;
 use vars qw( @ISA @EXPORT_OK );
 @ISA = qw( Exporter );
-@EXPORT_OK = qw( simple_location );
+@EXPORT_OK = qw( simple_location locationFromHomolBlock );
+
+sub new {
+    my( $pkg, $strand, @exons ) = @_;
+    
+    my $loc = bless [], $pkg;
+    
+    if ($strand) {
+        $loc->strand($strand);
+    }
+    if (@exons) {
+        $loc->exons(@exons);
+    }
+    
+    return $loc;
+}
+
+sub strand {
+    my( $loc, $strand ) = @_;
+    
+    if ($strand) {
+        confess "strand must be 'W' or 'C', not '$strand'"
+            unless $strand =~ /^[WC]$/;
+        $loc->[0] = $strand;
+    } else {
+        return $loc->[0];
+    }
+}
+
+sub exons {
+    my( $loc, @exons ) = @_;
+    
+    if (@exons) {
+        foreach (@exons) {
+            confess "Bad exon string '$_'" unless /^\d+\.\.\d+$/;
+        }
+        @$loc = ($loc->[0], @exons);
+    } else {
+        return @$loc[1..$#$loc];
+    }
+}
+
+sub locationFromHomolBlock {
+    my( $block, $score ) = @_;
+    $score ||= 200;
+        
+    # Divide the data up into forward and reverse strand sets
+    my( %strand );
+    foreach my $r (@$block) {
+        my( $score, $g_start, $g_end, $h_start, $h_end ) = @$r;
+        
+        if ($g_start < $g_end) {
+            push( @{$strand{'W'}}, [$score, $g_start, $g_end, $h_start, $h_end] );
+        } else {
+            push( @{$strand{'C'}}, [$score, $g_end, $g_start, $h_start, $h_end] );
+        }
+    }
+    
+    my( @set );
+    
+    # Divide each strand's set into hit blocks
+    foreach my $str (keys %strand) {    
+        my( @bits );
+        my $pos = undef;
+        my $dir = undef;
+        
+        foreach my $r (sort {$a->[1] <=> $b->[1]} @{$strand{$str}}) {            
+            # Get the direction of the match in the database hit
+            my $d = $r->[3] < $r->[4] ? 1 : 0;
+            $dir = $d unless defined $dir;
+            
+            # The end of the match is in a different field,
+            # depending up the direction.
+            my $p = $d ? $r->[4] : $r->[3];
+            $pos = $p unless defined $pos;
+            
+            # Data belongs in a new set if the direction is
+            # different to $dir or the position doesn't follow
+            # on from the previous position ($pos).
+            if (             ($d != $dir)
+                or (  $d and ($p < $pos))
+                or (! $d and ($p > $pos)) ) {
+                push( @set, [$str, @bits] );
+                @bits = ();
+                $dir = $d; # Set the new direction
+            }
+            $pos = $p; # Record the new end
+            push( @bits, $r );
+        }
+        push( @set, [$str, @bits] );
+    }
+    
+    my( @result );
+    foreach my $s (@set) {
+        my( $strand, @data ) = @$s;
+        
+        # Skip this set if its score isn't significant
+        my( $sum );
+        map { $sum += $_->[0] } @data;
+        next unless $sum >= $score;
+        
+        # Make a location string
+        my @exons = map "$_->[1]..$_->[2]", @data;
+        my $loc = Hum::EMBL::Location->new($strand, @exons);
+        my $str = $loc->format_location();
+        
+        # Get the start and end of the feature
+        my $start = @data[0]->[1];
+        my $end   = @data[$#data]->[2];
+        push( @result, [$start, $end, $str] );
+    }
+    return @result;
+}
 
 sub simple_location {
     my( $start, $end ) = @_;
@@ -93,7 +205,7 @@ sub _three_prime_arrow {
     $loc->[$#$loc] =~ s/(\d+)\.\.(\d+)/$1\.\.>$2/;
 }
 
-sub format {
+sub format_location {
     my( $location ) = @_;
     my( $direction, @exons ) = @$location;
     my $loc_string;
