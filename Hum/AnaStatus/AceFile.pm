d66a4e411d5a7dbcbc0363e0bf824edd83af1840
Javier Santoyo-Lopez <jsl@sanger.ac.uk>
2001-01-04 13:59:43 +0000

    implemented Sequence.pm and AceFile.pm

--- Hum/AnaStatus/AceFile.pm
+++ Hum/AnaStatus/AceFile.pm
@@ -4,16 +4,214 @@
 package Hum::AnaStatus::AceFile;
 
 use strict;
+use Carp;
+use Hum::Submission qw( prepare_statement );
 
 sub new {
-    my ( $pkg, $seq_name) = @_;
+    my ($pkg) = @_;
     
     my $self = bless {}, $pkg;
     
-   # return $self;
-        
+    return $self;
 }
 
+sub creation_time {
+    my ( $self, $creation_time ) = @_;
+
+    if ($creation_time) {
+        confess "Can't modify creation_time"
+            if $self->{'_creation_time'};
+        $self->{'_creation_time'} = $creation_time;
+    }
+    return $self->{'_creation_time'};
+}
+
+sub ana_seq_id {
+    my ( $self, $ana_seq_id ) = @_;
+
+    if ($ana_seq_id) {
+        confess "Can't modify ana_seq_id"
+            if $self->{'_ana_seq_id'};
+        $self->{'_ana_seq_id'} = $ana_seq_id;
+    }
+    return $self->{'_ana_seq_id'};
+}
+
+sub acefile_name {
+    my ( $self, $acefile_name ) = @_;
+
+    if ($acefile_name) {
+        confess "Can't modify acefile_name"
+            if $self->{'_acefile_name'};
+        $self->{'_acefile_name'} = $acefile_name;
+    }
+    return $self->{'_acefile_name'};
+}
+
+sub acefile_status_id {
+    my ( $self, $acefile_status_id ) = @_;
+
+    if ($acefile_status_id) {
+        confess "Can't modify acefile_status_id"
+            if $self->{'_acefile_status_id'};
+        $self->{'_acefile_status_id'} = $acefile_status_id;
+    }
+    return $self->{'_acefile_status_id'};
+}
+
+sub task_name {
+    my ( $self, $task_name ) = @_;
+
+    if ($task_name) {
+        confess "Can't modify task_name"
+            if $self->{'_task_name'};
+        $self->{'_task_name'} = $task_name;
+    }
+    return $self->{'_task_name'};
+}
+
+{
+    my( $store_acefile );
+
+    sub store {
+        my( $self ) = @_;
+
+        my $acefile_name = $self->acefile_name
+            or confess "acefile_name is empty";
+        my $acefile_status_id = $self->acefile_status_id
+            or confess "acefile_status_id is empty";
+        my $ana_seq_id = $self->ana_seq_id
+            or confess "ana_seq_id is empty";
+        my $task_name = $self->task_name
+            or confess "task_name is empty";
+        my $creation_time = $self->creation_time;
+        unless ($creation_time) {
+            $creation_time = time;
+            $self->creation_time($creation_time);
+        }
+
+        $store_acefile ||= prepare_statement(q{
+            INSERT ana_acefile( acefile_name
+              , acefile_status_id
+              , ana_seq_id
+              , task_name
+              , creation_time
+            VALUES(?,?,?,?,?,FROM_UNIXTIME(?))
+            });
+        $store_acefile->execute(
+            $acefile_name,
+            $acefile_status_id,
+            $ana_seq_id,
+            $task_name,
+            $creation_time);
+        
+        $self->store_acefile_date_range;
+    }
+}
+
+{
+    my( $get_acefile_date,
+        $add_new_acefile_date,
+        $update_latest_date,
+        $update_earliest_date,
+        $add_dates );
+    
+    sub store_acefile_date_range {
+        my( $self ) = @_;
+
+        my $time = $self->creation_time;
+        my $name = $self->acefile_name;
+        
+        $get_acefile_date ||= prepare_statement(q{
+            SELECT UNIX_TIMESTAMP(earliest_date)
+              , UNIX_TIMESTAMP(latest_date)
+            FROM acefile_name_date
+            WHERE acefile_name = ?
+            });
+        $get_acefile_date->execute($name);
+        my($earliest, $latest) = $get_acefile_date->fetchrow;
+
+        # Update the row if we have a later date
+        # than the latest stored in the database
+        if ($earliest) {
+            if ($time > $latest) {
+                $update_latest_date ||= prepare_statement(q{
+                    UPDATE acefile_name_date
+                    SET latest_date = FROM_UNIXTIME(?)
+                    WHERE acefile_name = ?
+                    });
+                $update_latest_date->execute($time, $name);
+            }
+            if ($time < $earliest) {
+                $update_earliest_date ||= prepare_statement(q{
+                    UPDATE acefile_name_date
+                    SET earliest_date = FROM_UNIXTIME(?)
+                    WHERE acefile_name = ?
+                    });
+                $update_earliest_date->execute($time, $name);
+            }
+        } else {
+            # Add a new row
+            confess "$name latest date: $latest. Earliest date not defined"
+                if $latest;
+            $add_dates ||= prepare_statement(q{
+                INSERT acefile_name_date( acefile_name
+                      , earliest_date
+                      , latest_date )
+                VALUES (?,FROM_UNIXTIME(?),FROM_UNIXTIME(?))                
+                });
+            $update_latest_date->execute($name, $time, $time);
+        }
+    }
+}
+
+
+{
+    my %valid_ace_status_id;
+    
+    sub is_valid_ace_status_id {
+        my ($self, $ace_status_id) = @_;
+        
+        my @valid_ace_status_id;
+                
+        unless (%valid_ace_status_id){
+            my $sth = prepare_statement (q{
+            SELECT acefile_status_id
+            FROM ana_acefile_status_dict });
+            
+            $sth->execute;
+            
+            while (my $valid_ace_status_id = $sth->fetchrow) {
+                push (@valid_ace_status_id, $valid_ace_status_id);
+            }                        
+            %valid_ace_status_id = map {$_, 1} @valid_ace_status_id;
+        }
+        return $valid_ace_status_id{$ace_status_id};
+    }
+}
+
+{
+    my %valid_task_name;
+    
+    sub is_valid_task_name {
+        my ($self, $task_name) = @_;
+        
+        my @valid_task_name;
+                
+        unless (%valid_task_name){
+            my $sth = prepare_statement (q{
+            SELECT task_name
+            FROM ana_task });
+                        
+            $sth->execute;            
+            while (my $valid_task_name = $sth->fetchrow) {
+                push (@valid_task_name, $valid_task_name);
+            }                        
+            %valid_task_name = map {$_, 1} @valid_task_name;
+        }
+        return $valid_task_name{$task_name};
+    }
+}
 
 1;
 
--- Hum/AnaStatus/Sequence.pm
+++ Hum/AnaStatus/Sequence.pm
@@ -10,8 +10,6 @@ use Hum::AnaStatus::AceFile;
 
 sub new_from_sequence_name {
     my ($pkg, $seq_name) = @_;
-    
-    my $self = bless {}, $pkg;
 
     my $sth = prepare_statement(q{
         SELECT a.analysis_directory
@@ -21,6 +19,7 @@ sub new_from_sequence_name {
           , status.status_id
           , status.status_date
           , s.embl_checksum
+          , person.annotator_uname
         FROM sequence s
           , ana_sequence a
         LEFT JOIN ana_status status
@@ -35,15 +34,34 @@ sub new_from_sequence_name {
 
     $sth->execute($seq_name);
     
-    # The reference of the first array refers to the row, and the second 
-    # array referes to the value of each attribute in the row.
-    # values are in the same order than in the select statement
+    # in $ans the reference of the first array refers to the row, and the second 
+    # array refers to the value of each attribute in the row.
+    # values are in the same order than in the SELECT statement
     my $ans = $sth->fetchall_arrayref;
     
-    if (@$ans == 1) {
-       # Make new object and return          
-       my $self = bless $ans->[0], $pkg;              
-       return $self;
+    if (@$ans == 1) {                             
+       my (
+            $analysis_directory,
+            $analysis_priority,
+            $seq_id,
+            $ana_seq_id,
+            $status_id,
+            $status_date,
+            $embl_checksum,
+            $annotator_uname
+           ) = @{$ans->[0]};
+        
+        my $self = bless {}, $pkg;
+        
+        $self->analysis_directory($analysis_directory);
+        $self->analysis_priority($analysis_priority);
+        $self->seq_id($seq_id);
+        $self->ana_seq_id($ana_seq_id);
+        $self->status_id($status_id || 0);
+        $self->status_date($status_date || 0);
+        $self->embl_checksum($embl_checksum);
+        $self->annotator_uname($annotator_uname);
+        return $self;
     }
     elsif (@$ans > 1) {
         my $rows = @$ans;
@@ -58,62 +76,263 @@ sub new_from_sequence_name {
     }
 }
 
-sub analysis_directory {
-    my ( $self, $new_analysis_directory ) = @_;
-    my ( $analysis_directory ) = $self->[0];
-            
-    if ($new_analysis_directory) {
-        $self->[0] = $new_analysis_directory;
-    } elsif ($analysis_directory) {
-        return $analysis_directory;
-    } else {
-        confess "Analysis directory not found";
+{
+    my( $set_not_current, $new_status );
+
+    sub set_status {
+        my ( $self, $status ) = @_;
+        
+        my $time = time;
+        
+        confess "status id not defined" unless $status;
+        confess "Sequence already has status '$status'"
+            if $status == $self->status;
+        
+        #$status =~ /^\d+$/ or confess "Illegal status_id '$status'";
+
+        my $ana_seq_id = $self->ana_seq_id
+            or confess "No ana_seq_id in object";
+
+        $set_not_current ||= prepare_statement(q{
+            UPDATE ana_status
+            SET is_current = 'N'
+            WHERE ana_seq_id = ?
+            });
+
+        $new_status ||= prepare_statement(q{
+            INSERT ana_status( ana_seq_id
+                  , is_current
+                  , status_date
+                  , status_id )
+            VALUES( ?
+                  , 'Y'
+                  , FROM_UNIXTIME(?)
+                  , ?)
+            });
+        
+        $set_not_current->execute($ana_seq_id);
+        $new_status->execute($ana_seq_id, $time, $status);
+        my $rows = $new_status->rows;
+        if ($rows == 1) {
+            $self->{'_status_id'}   = $status;
+            $self->{'_status_date'} = $time;
+            return 1;
+        } else {
+            confess "ana_status INSERT failed";
+        }
     }
-
-}
-sub analysis_priority {
-    my ( $self, $new_analysis_priority ) = @_ ;
-    
-    my ( $analysis_priority ) = $self->[1];
-    return $analysis_priority if $analysis_priority;
 }
 
-sub seq_id {
-    my ( $self, $new_seq_id ) = @_;
+{
+    my $set_annotator_uname;
     
-    my ( $seq_id ) = $self->[2];
-    return $seq_id if $seq_id;
-}
+    sub set_annotator_uname {
+        my ($self, $annotator_uname ) = @_;
+        
+        confess "missing annotator_uname" unless $annotator_uname;
+        $self->annotator_uname($annotator_uname);##?
+        
+        my $ana_seq_id = $self->ana_seq_id
+            or confess "No ana_seq_id in object";
+        
+        $set_annotator_uname ||= prepare_statement(q{            
 
-sub ana_seq_id {
-    my ( $self, $new_ana_seq_id ) = @_;
-    
-    my ( $ana_seq_id )= $self->[3];
-    return $ana_seq_id if $ana_seq_id;
+            INSERT ana_sequence_person (annotator_uname
+                  , ana_seq_id)
+            VALUES(?,?)
+            
+            });
+            
+        $set_annotator_uname->execute($annotator_uname, $ana_seq_id);
+                
+    }
 }
+ 
+ 
 
 sub status_id {
-    my ( $self, $new_status_id ) = @_;
+    my ( $self, $status_id ) = @_;
     
-    my ( $status_id ) = $self->[4];
-    return $status_id if $status_id;
+    if ($status_id) {
+        confess "Unknown status_id '$status_id'"
+            unless $self->is_valid_status_id($status_id);
+        confess "Can't modify status_id"
+            if $self->{'_status_id'};
+        $self->{'_status_id'} = $status_id;
+    }
+    return $self->{'_status_id'};
 }
+
+
+{
+   my %valid_status_id;
+    
+    sub is_valid_status_id {
+        my ($self, $status_id) = @_;
+        
+        my @valid_status_id;
+                
+        unless (%valid_status_id){
+            my $sth = prepare_statement (q{
+            SELECT status_id
+            FROM ana_status_dict });
+            
+            $sth->execute;
+            
+            while (my $valid_status_id = $sth->fetchrow) {
+                push (@valid_status_id, $valid_status_id);
+            }                        
+            %valid_status_id = map {$_, 1} @valid_status_id;
+        }
+        return $valid_status_id{$status_id};
+    }
+}
+
+
+sub analysis_directory {
+    my ( $self, $analysis_directory ) = @_;
+    
+    if ($analysis_directory) {
+        confess "Can't modify analysis_directory"
+            if $self->{'_analysis_directory'};
+        $self->{'_analysis_directory'} = $analysis_directory;
+    }
+    return $self->{'_analysis_directory'};
+}
+
+
+sub analysis_priority {
+    my ( $self, $analysis_priority ) = @_;
+    
+    if (defined $analysis_priority) {
+        confess "Can't modify analysis_priority"
+            if $self->{'_analysis_priority'};
+        $self->{'_analysis_priority'} = $analysis_priority;
+    }
+    return $self->{'_analysis_priority'};
+}
+
+
+sub seq_id {
+    my ( $self, $seq_id ) = @_;
+    
+    if ($seq_id) {
+        confess "Can't modify seq_id"
+            if $self->{'_seq_id'};
+        $self->{'_seq_id'} = $seq_id;
+    }
+    return $self->{'_seq_id'};
+}
+
+
+sub ana_seq_id {
+    my ( $self, $ana_seq_id ) = @_;
+    
+    if ($ana_seq_id) {
+        confess "Can't modify ana_seq_id"
+            if $self->{'_ana_seq_id'};
+        $self->{'_ana_seq_id'} = $ana_seq_id;
+    }
+    return $self->{'_ana_seq_id'};
+}
+
 
 sub status_date {
-    my ( $self, $new_status_date ) = @_;
+    my ( $self, $status_date ) = @_;
     
-    my ( $status_date ) = $self->[5];
-    return $status_date if $status_date;
+    if ($status_date) {
+        confess "Can't modify status_date"
+            if $self->{'_status_date'};
+        $self->{'_status_date'} = $status_date;
+    }
+    return $self->{'_status_date'};
 }
+
 
 sub embl_checksum {
-    my ( $self, $new_embl_checksum ) = @_ ;
+    my ( $self, $embl_checksum ) = @_;
     
-    my ( $embl_checksum ) = $self->[6];
-    return $embl_checksum if $embl_checksum;
+    if ($embl_checksum) {
+        confess "Can't modify embl_checksum"
+            if $self->{'_embl_checksum'};
+        $self->{'_embl_checksum'} = $embl_checksum;
+    }
+    return $self->{'_embl_checksum'};
 }
 
 
+sub annotator_uname {
+    my ( $self, $annotator_uname ) = @_;
+    
+    if ($annotator_uname) {
+        confess "Unknown annotator '$annotator_uname'"
+            unless $self->is_valid_annotator($annotator_uname);
+        confess "Can't modify annotator_uname"
+            if $self->{'_annotator_uname'};
+        $self->{'_annotator_uname'} = $annotator_uname;
+    }
+    return $self->{'_annotator_uname'};
+}
+
+{
+    my( %valid_annotators );
+
+    sub is_valid_annotator {
+        my( $self, $annotator ) = @_;
+                
+        unless (%valid_annotators){
+            my @valid_annotators;
+            my $sth = prepare_statement (q{
+            SELECT annotator_uname
+            FROM ana_person });
+            
+            $sth->execute;
+            
+            while (my $valid_annotator = $sth->fetchrow) {
+                push (@valid_annotators, $valid_annotator);
+            }                        
+            %valid_annotators = map {$_, 1} @valid_annotators;
+        }
+        return $valid_annotators{$annotator};
+    }
+}
+
+{
+    my( $get_acefile_data );
+
+    sub get_all_Acefiles {
+        my ($self) = @_;
+
+        my $ana_seq_id = $self->ana_seq_id
+            or confess "No ana_seq_id in object";
+
+        $get_acefile_data ||= prepare_statement(q{
+            SELECT acefile_name
+              , acefile_status_id
+              , task_name
+              , creation_time
+            FROM ana_acefile
+            WHERE ana_seq_id = ?
+            });
+        $get_acefile_data->execute($ana_seq_id);
+        
+        my( @acefiles );
+        while ( my($acefile_name,
+            $acefile_status_id,
+            $task_name,
+            $creation_time ) = $get_acefile_data->fetchrow) {
+                my $self = Hum::AnaStatus::AceFile->new;
+                $self->ana_seq_id($ana_seq_id);
+                $self->acefile_name($acefile_name);
+                $self->acefile_status_id($acefile_status_id);
+                $self->task_name($task_name);
+                $self->creation_time($creation_time);
+                                
+                push(@acefiles, $self);
+        }
+        return @acefiles;
+    }
+}
 
 1;
 
@@ -123,7 +342,7 @@ __END__
 
 =head1 NAME - Hum::AnaStatus::Sequence
 
-=head1 MEHTHODS
+=head1 METHODS
 
 =over 4
 
@@ -164,15 +383,15 @@ their fields, they dont' allow you to set them
 
 =item seq_id
 
-=item current_status
+=item status_id
 
 The number of the status currently held
 
-=item current_status_date
+=item status_date
 
 The date the current status was assigned.
 
-=item current_status_description
+=item status_description
 
 The description of this status
 
