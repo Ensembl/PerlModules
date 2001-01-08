68ecf80b379530c42176a33574151617f2eaa905
James Gilbert <jgrg@sanger.ac.uk>
2001-01-08 14:55:31 +0000

    *** empty log message ***

---
+++ Hum/ProjectDump/EMBL/HGMP.pm
@@ -0,0 +1,31 @@
+
+### Hum::ProjectDump::EMBL::HGMP
+
+package Hum::ProjectDump::EMBL::HGMP;
+
+use strict;
+use Hum::ProjectDump::EMBL;
+use Hum::EmblUtils qw( add_source_FT
+                       add_Organism
+                       species_binomial
+                       );
+use Hum::EMBL::Utils qw( EMBLdate );
+
+use vars '@ISA';
+@ISA = 'Hum::ProjectDump::EMBL';
+
+sub make_embl {
+    
+}
+
+
+1;
+
+__END__
+
+=head1 NAME - Hum::ProjectDump::EMBL::HGMP
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
