8fcb8ab237a7099693a756c216b4635afe40daa6
James Gilbert <jgrg@sanger.ac.uk>
2002-05-09 16:35:58 +0000

    *** empty log message ***

--- Hum/Conf.pm
+++ Hum/Conf.pm
@@ -79,8 +79,7 @@ my $humace_queue = "$humpub/humace/queue";
     SPECIES_LIST =>  [keys %$ftp_structure],
 
     PFETCH_SERVER_LIST => [
-        [qw{ plato.sanger.ac.uk    22100 }],
-        [qw{ infosrv1.sanger.ac.uk 22100 }],
+        [qw{ pubseq.sanger.ac.uk    22100 }],
         ],
 
     HUMACE_DIR    => "/nfs/humace/humpub/humace",
