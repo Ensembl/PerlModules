a05f99cc23c89b839243490bc7875b6f1e396653
Tim Hubbard <th@sanger.ac.uk>
2002-11-18 18:06:12 +0000

    add VIEW_VEGADB to SubmissionConf.pm

--- Hum/SubmissionConf.pm
+++ Hum/SubmissionConf.pm
@@ -4,8 +4,15 @@ package Hum::SubmissionConf;
 use strict;
 
 sub localisation{
-    my $host   = 'humsrv1';
-    my $port   = 3399;
+     # allow sanger people to view what is on vegadb if 'VIEW_VEGADB' set
+    my($host,$port);
+    if($ENV{'VIEW_VEGADB'}){
+	$host   = 'vegadb.sanger.ac.uk';
+	$port   = 3306;
+    }else{
+	$host   = 'humsrv1';
+	$port   = 3399;
+    }
     return($host,$port);
 }
 
