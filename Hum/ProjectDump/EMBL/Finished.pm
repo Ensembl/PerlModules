e8ae1bf8aa81019441740fef1fe7eff3d1ffdecf
James Gilbert <jgrg@sanger.ac.uk>
2003-04-22 10:57:35 +0000

    *** empty log message ***

--- Hum/ProjectDump/EMBL/Finished.pm
+++ Hum/ProjectDump/EMBL/Finished.pm
@@ -288,7 +288,8 @@ http://www.sanger.ac.uk/Projects/C_elegans/wormpep');
 'Repeat names beginning "Dr" were identified by the Recon repeat discovery
 system (Zhirong Bao and Sean Eddy, submitted), and those beginning "drr"
 were identified by Rick Waterman (Stephen Johnson lab, WashU).  For
-further information see http://www/Projects/D_rerio/fishmask.shtml';
+further information see
+http://www.sanger.ac.uk/Projects/D_rerio/fishmask.shtml';
 
     sub add_standard_CC {
         my( $pdmp, $embl ) = @_;
