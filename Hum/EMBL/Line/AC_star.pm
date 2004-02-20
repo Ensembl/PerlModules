93972c72d27c4b5fc2a73eb13d07f1aa77642bd2
James Gilbert <jgrg@sanger.ac.uk>
2004-02-20 16:43:24 +0000

    *** empty log message ***

--- Hum/EMBL/Line/AC_star.pm
+++ Hum/EMBL/Line/AC_star.pm
@@ -21,9 +21,34 @@ sub _compose {
     
     my $identifier = $line->identifier;
     confess "Identifier '$identifier' too long"
-        if length($identifier) > 75;
+        if length($identifier) > 35;
     
     return "AC * $identifier\n";
 }
 
 1;
+
+__END__
+
+=head1 Hum::EMBL::Line::AC_star
+
+This is the line handling module for lines in
+EMBL files that begin 'AC * '.   These are used
+to tag our submissions to EMBL.  Once the entry
+has been processed we can get the accession
+assigned to it from the EMBL oracle database
+using the project number (which is "12" for the
+"sangerhs" entries) concatenated to the
+identifier as a lookup.
+
+AC_star has one scalar field:
+
+=over 4
+
+=item identifier
+
+The identifier should begin with an underscore,
+contain only capital letters and numerals, and
+not exceed 35 characters.
+
+=back
---
+++ Hum/Sort.pm
@@ -0,0 +1,70 @@
+
+### Hum::Sort
+
+package Hum::Sort;
+
+use strict;
+use base 'Exporter';
+
+our @EXPORT_OK = qw{ ace_sort };
+
+sub ace_sort {
+    my $A = [ split(/(\d+)/, lc shift) ];
+    my $B = [ split(/(\d+)/, lc shift) ];
+    
+    return _ace_array_sort($A, $B);
+}
+
+sub _ace_array_sort {
+    my( $A, $B ) = @_;
+    
+    my $x = shift @$A;
+    my $y = shift @$B;
+    
+    # $x or $y will be undef once we are off
+    # the end of either array.
+    # undef (shorter) sorts first.
+    if (! defined $x) {
+        return defined $y ? -1 : 0;
+    }
+    elsif (! defined $y) {
+        # $x must be defined, or condition
+        # above would have matched.
+        return 1;
+    }
+    # Next two conditions recursively call _ace_array_sort
+    # if the comparison operators return 0 (equal).
+    elsif ($x =~ /\d/ and $y =~ /\d/) {
+        # Compare numerically if both elements are numeric
+        return $x <=> $y || _ace_array_sort($A, $B);
+    }
+    else {
+        # cmp does the right thing otherwise
+        # (numbers are smaller than letters)
+        return $x cmp $y || _ace_array_sort($A, $B);
+    }
+}
+
+
+
+
+1;
+
+__END__
+
+=head1 NAME - Hum::Sort
+
+=head1 SYNOPSIS
+
+    use Hum::Sort 'ace_sort';
+    
+    # Sorting strings
+    @sorted = { ace_sort($a, $b) } @un_sorted;
+    
+    # Sorting objects
+    @sorted_obj = { ace_sort($a->name, $b->name) } @objects;
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
