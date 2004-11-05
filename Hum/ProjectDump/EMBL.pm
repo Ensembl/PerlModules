ef7159fbd9bc35985c57faaa033798afb8824512
Chao-Kung Chen <ck1@sanger.ac.uk>
2004-11-05 14:56:42 +0000

    # CC lines     $pdmp->add_Headers($embl, $contig_map);     #$embl->newXX; # this becomes redundant to the CC lines code of Factory.pm, otherwise 2 XX lines

--- Hum/ProjectDump/EMBL.pm
+++ Hum/ProjectDump/EMBL.pm
@@ -97,7 +97,7 @@ sub make_embl {
 
     # CC lines
     $pdmp->add_Headers($embl, $contig_map);
-    $embl->newXX;
+    #$embl->newXX; # this becomes redundant to the CC lines code of Factory.pm, otherwise 2 XX lines
 
     # Feature table header
     $embl->newFH;
