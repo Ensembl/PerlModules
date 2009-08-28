39c8370f8fb6bf60d452a56a1c01b15abc824108
Mustapha Larbaoui <ml6@sanger.ac.uk>
2009-08-28 14:29:37 +0000

    make only one prepare statement to oracle

--- Hum/CloneProject.pm
+++ Hum/CloneProject.pm
@@ -12,33 +12,31 @@ use vars qw( @ISA @EXPORT_OK );
 @EXPORT_OK = qw(fetch_projectname_from_clonename
                 fetch_project_status);
 
+my $cp  = prepare_track_statement(qq{
+			SELECT projectname
+            FROM clone_project
+            WHERE clonename = ?
+          });
+
+my $p_status = prepare_track_statement(qq{
+					SELECT psd.description, ps.statusdate
+                    FROM project_status ps, projectstatusdict psd
+                    WHERE ps.projectname = ?
+                    AND ps.status=psd.id_dict
+                    AND ps.iscurrent = 1
+               });
 
 sub fetch_projectname_from_clonename {
   my ($clonename) = @_;
-
-  my $cp  =
-    prepare_track_statement(qq{SELECT projectname
-                                 FROM clone_project
-                                 WHERE clonename = ?
-                               });
   $cp->execute($clonename);
-  return $cp->fetchrow;
 
+  return $cp->fetchrow;
 }
 
 sub fetch_project_status {
-
   my ($projectname) = @_;
-
-  my $p_status =
-    prepare_track_statement(qq{SELECT psd.description, ps.statusdate
-                               FROM project_status ps,
-                                    projectstatusdict psd
-                               WHERE ps.projectname = ?
-                               AND ps.status=psd.id_dict
-                               AND ps.iscurrent = 1
-                               });
   $p_status->execute($projectname);
+
   return  my($status, $statusdate) = $p_status->fetchrow;
 }
 
