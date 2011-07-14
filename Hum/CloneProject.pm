75b4303cdd141f5367b4f90c41d0d54e4abbd9db
Matthew Astley <mca@sanger.ac.uk>
2011-07-14 11:02:54 +0000

    defer prepare_track_statement calls.
    they were opening database connections eagerly & early during compilation

--- Hum/CloneProject.pm
+++ Hum/CloneProject.pm
@@ -12,32 +12,38 @@ use vars qw( @ISA @EXPORT_OK );
 @EXPORT_OK = qw(fetch_projectname_from_clonename
                 fetch_project_status);
 
-my $cp  = prepare_track_statement(qq{
+my ($cp, $p_status);
+
+sub _cp {
+    return $cp ||= prepare_track_statement(qq{
 			SELECT projectname
             FROM clone_project
             WHERE clonename = ?
           });
+}
 
-my $p_status = prepare_track_statement(qq{
+sub _p_status {
+    return $p_status ||= prepare_track_statement(qq{
 					SELECT psd.description, ps.statusdate
                     FROM project_status ps, projectstatusdict psd
                     WHERE ps.projectname = ?
                     AND ps.status=psd.id_dict
                     AND ps.iscurrent = 1
                });
+}
 
 sub fetch_projectname_from_clonename {
   my ($clonename) = @_;
-  $cp->execute($clonename);
+  _cp()->execute($clonename);
 
   return $cp->fetchrow;
 }
 
 sub fetch_project_status {
   my ($projectname) = @_;
-  $p_status->execute($projectname);
+  _p_status()->execute($projectname);
 
-  return  my($status, $statusdate) = $p_status->fetchrow;
+  return  my($status, $statusdate) = _p_status()->fetchrow;
 }
 
 
