bb118aca731d075bad3a5d42102cc334a5154d61
James Gilbert <jgrg@sanger.ac.uk>
2011-08-12 10:08:15 +0000

    new multiplexed clone gap5 module

---
+++ Hum/ProjectDump/EMBL/Unfinished/Multiplexed.pm
@@ -0,0 +1,25 @@
+
+### Hum::ProjectDump::EMBL::Unfinished::Multiplexed
+
+package Hum::ProjectDump::EMBL::Unfinished::Multiplexed;
+
+use strict;
+use warnings;
+use Carp;
+use Hum::FastqFileIO;
+
+use base 'Hum::ProjectDump::EMBL::Unfinished';
+
+
+
+
+1;
+
+__END__
+
+=head1 NAME - Hum::ProjectDump::EMBL::Unfinished::Multiplexed
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
