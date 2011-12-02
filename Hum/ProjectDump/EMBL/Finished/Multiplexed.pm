f616a48767fcea1b08b57ce19ba000266ceceb4a
James Gilbert <jgrg@sanger.ac.uk>
2011-12-02 11:57:17 +0000

    new module for MULTIPLEXED projects

---
+++ Hum/ProjectDump/EMBL/Finished/Multiplexed.pm
@@ -0,0 +1,22 @@
+
+### Hum::ProjectDump::EMBL::Finished::Multiplexed
+
+package Hum::ProjectDump::EMBL::Finished::Multiplexed;
+
+use strict;
+use warnings;
+
+use base qw{ Hum::ProjectDump::EMBL::Finished };
+
+
+
+1;
+
+__END__
+
+=head1 NAME - Hum::ProjectDump::EMBL::Finished::Multiplexed
+
+=head1 AUTHOR
+
+James Gilbert B<email> jgrg@sanger.ac.uk
+
