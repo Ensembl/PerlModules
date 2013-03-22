124f917cafb2302d314537ea02bfae7b42ba4b72
Jeremy Henty <jh13@sanger.ac.uk>
2013-03-22 16:11:44 +0000

    Hum/Ace/SeqFeature/Simple.pm: move ZMap XML code to ensembl-otter

--- Hum/Ace/SeqFeature/Simple.pm
+++ Hum/Ace/SeqFeature/Simple.pm
@@ -54,20 +54,6 @@ sub ace_string {
       $self->score, $self->text;
 }
 
-sub zmap_xml_feature_tag {
-    my ($self, $offset) = @_;
-    
-    $offset ||= 0;
-    
-    return sprintf qq{<feature name="%s" start="%d" end="%d" strand="%s" style="%s" score="%.3f"></feature>\n},
-        $self->text,
-        $offset + $self->seq_start,
-        $offset + $self->seq_end,
-        $self->seq_strand == -1 ? '-' : '+',
-        $self->Method->style_name,
-        $self->score;
-}
-
 
 1;
 
