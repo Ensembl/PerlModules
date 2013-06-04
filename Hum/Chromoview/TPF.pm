c42418ca51240350d209dcc44a0e4036b86da983
James Torrance <jt8@sanger.ac.uk>
2013-06-04 17:19:44 +0000

    New module to handle retrieval of TPF/AGP data for Chromoview.

---
+++ Hum/Chromoview/TPF.pm
@@ -0,0 +1,86 @@
+package Hum::Chromoview::TPF;
+
+### Author: jt8@sanger.ac.uk
+
+use strict;
+use warnings;
+use Hum::TPF;
+use Hum::AGP;
+
+sub new {
+    my ($class, $species, $chromosome, $subregion) = @_;
+    my $self = {
+        '_species' => $species,
+        '_chromosome' => $chromosome,
+        '_subregion' => $subregion,
+    };
+    return bless ($self, $class);
+}
+
+sub species {
+    my ($self) = @_;
+    return $self->{'_species'};
+}
+
+sub chromosome {
+    my ($self) = @_;
+    return $self->{'_chromosome'};
+}
+
+sub subregion {
+    my ($self) = @_;
+    return $self->{'_subregion'};
+}
+
+sub tpf {
+    my ($self) = @_;
+    
+    if(exists($self->{'_tpf'})) {
+        return $self->{'_tpf'};
+    }
+    else {
+        $self->prepare_tpf_agp;
+    }
+}
+
+sub agp {
+    my ($self) = @_;
+    
+    if(exists($self->{'_agp'})) {
+        return $self->{'_agp'};
+    }
+    else {
+        $self->prepare_tpf_agp;
+    }
+}
+
+sub prepare_tpf_agp {
+
+    my($self) = @_;
+
+    # fetch TPF
+    my $tpf = $self->subregion ? Hum::TPF->current_from_species_chromsome_subregion($self->species, $self->chromosome, $self->subregion) :
+    Hum::TPF->current_from_species_chromsome($self->species, $self->chromosome);
+
+    # fetch AGP
+    my $agp = Hum::AGP->new;
+    $agp->allow_dovetails(1);
+    $agp->catch_errors(1);
+    $agp->allow_unfinished(1);
+    $agp->min_htgs_phase(2);
+    $agp->chr_name($self->chromosome);
+    $agp->verbose(0);
+    $agp->process_TPF($tpf);
+
+    $self->{'_tpf'} = $tpf;
+    $self->{'_agp'} = $agp;
+
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+James Torrance email B<jt8@sanger.ac.uk>
