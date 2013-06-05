44ea4f5b45d822cd1cfa0be1ddcc0e6a81e8c5ce
James Torrance <jt8@sanger.ac.uk>
2013-06-05 11:33:31 +0000

    Added project status details.

--- Hum/Chromoview/TPF/Row.pm
+++ Hum/Chromoview/TPF/Row.pm
@@ -58,8 +58,25 @@ sub build_library_and_clone {
 	my ($clonename, $lib) = $self->row->get_sanger_clone_and_libraryname_from_intl_name($self->row->intl_clone_name);
 	$lib =~ s/_/ /g;
 	$lib = '-' unless $lib;
+	
+    # some clones may have been splitted, use sequence table for projectname
+	# instead of Hum::CloneProject::fetch_projectname_from_clonename($clonaname)
+	# as this is querying clone_project table, which may not have relevant info
+	my $projname;
+	eval {
+    	$projname = Hum::CloneProject::fetch_projectname_from_clonename($clonename) ||
+        	$self->row->SequenceInfo->projectname;
+	};
+    $clonename = $self->row->sanger_clone_name unless $clonename;
+    if ( defined $projname and defined $clonename and $projname ne $clonename ) {
+    	my $swap = "Clone ($clonename) - Project ($projname) swap";
+    	$clonename = $swap;
+	}
+    
+	
 	$self->{'_library'} = $lib;
 	$self->{'_clonename'} = $clonename;
+	$self->{'_projectname'} = $projname;
     
     return;
 }
@@ -86,6 +103,45 @@ sub clonename {
     return $self->{'_clonename'};
 }
 
+sub projectname {
+    my ($self) = @_;
+    
+    if(!exists($self->{'_projectname'})) {
+        $self->build_library_and_clone;
+    }
+    return $self->{'_projectname'};
+}
+
+sub project_status {
+    my ($self) = @_;
+    
+    if(!exists($self->{'_project_status'})) {
+        $self->build_status;
+    }
+    return $self->{'_project_status'};
+}
+
+sub project_status_date {
+    my ($self) = @_;
+    
+    if(!exists($self->{'_project_status_date'})) {
+        $self->build_status;
+    }
+    return $self->{'_project_status_date'};
+}
+
+sub build_status {
+    my ($self) = @_;
+    
+    my ($status, $statusdate) = Hum::CloneProject::fetch_project_status($self->projectname);
+    $status = $statusdate = '-' unless $self->projectname;
+    
+    $self->{'_project_status'} = $status;
+    $self->{'_project_status_date'} = $statusdate;
+    
+    return;
+}
+
 sub data_for_chromoview {
     my ($self) = @_;
     
@@ -100,12 +156,14 @@ sub data_for_chromoview {
                 contig=>$self->row->contig_name || '?',
                 external_clone=>$self->row->intl_clone_name || '?',
                 internal_clone=>$self->clonename,
-                status=>undef,
+                project_status=>$self->project_status,
+                project_status_date=>$self->project_status_date,
                 accession_and_finishing=>$self->row->accession . "/" . $self->finishing_status,
                 length=> $self->sequence_length,
                 library=> $self->library,
                 
                 accession=>$self->row->accession,
+                projectname=>$self->projectname,
         };
     }
 }
